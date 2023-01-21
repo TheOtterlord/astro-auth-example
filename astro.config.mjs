fc1c832a987c35bfac74fc9005359f8792d43ca3
= <otterlord.dev@gmail.com>
2023-01-21 14:48:20 +0000

    Redeploy working version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+// NOTE: something weird is happening in edge, but it's not our fault
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
