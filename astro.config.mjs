55951efceca92036868b4899eccf18a78dee17db
= <otterlord.dev@gmail.com>
2023-01-27 16:31:30 +0000

    Update auth package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    mode: 'standalone'
+  }),
 });
