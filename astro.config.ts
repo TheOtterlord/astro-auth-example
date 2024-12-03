af3c298a634baa8eac2661e246deaed55328a47b
Reuben Tier <otterlord.dev@gmail.com>
2024-12-03 20:59:00 +0000

    Update to Astro 5

--- astro.config.ts
+++ astro.config.ts
@@ -5,8 +5,6 @@ import auth from 'auth-astro'
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [
-    auth()
-  ],
-  adapter: vercel(),
+  integrations: [auth()],
+  adapter: vercel({}),
 });
